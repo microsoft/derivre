690cda2eca0eda144187376ea6744623ecf7d14e
Hudson Cooper <hncooper96@gmail.com>
2025-01-02 12:48:02 -0800

    early exit when guaranteed a solution

--- src/remainder.rs
+++ src/remainder.rs
@@ -4,17 +4,9 @@ pub fn check_remainder(divisor: u32, remainder: u32, n: u32) -> bool {
     // Normalize remainder
     let remainder = remainder % divisor;
 
-    // Short circuit if we are out of digits
-    if n == 0 {
-        return remainder == 0;
-    };
-    // Short circuit if we can't possibly reach remainder
-    if 10_u32.pow(n) - 1 < remainder {
-        return false;
-    }
-    // Short circuit if n is sufficiently large
-    if ((divisor as f32).log10().ceil() as u32) < n {
-        return true;
+    // Simple cases that don't require DP
+    if let Some(result) = check_remainder_simple(divisor, remainder, n) {
+        return result;
     }
 
     // 1d DP table
@@ -23,7 +15,7 @@ pub fn check_remainder(divisor: u32, remainder: u32, n: u32) -> bool {
     let mut next = vec![false; divisor as usize];
     current[0] = true; // 0 is always achievable
 
-    for _ in 0..n {
+    for i in 0..n {
         // Clear the 'next' array for the current iteration
         for entry in next.iter_mut() {
             *entry = false;
@@ -32,8 +24,12 @@ pub fn check_remainder(divisor: u32, remainder: u32, n: u32) -> bool {
             if current[rem as usize] {
                 for digit in 0..=9 {
                     let new_rem = (rem * 10 + digit) % divisor;
-                    if new_rem == remainder {
-                        // Short circuit if we've found a solution
+                    // Short circuit if we're guaranteed a solution
+                    if let Some(true) = check_remainder_simple(
+                        divisor,
+                        (remainder + divisor - new_rem) % divisor,
+                        n - i - 1,
+                    ) {
                         return true;
                     }
                     next[new_rem as usize] = true;
@@ -44,4 +40,21 @@ pub fn check_remainder(divisor: u32, remainder: u32, n: u32) -> bool {
         std::mem::swap(&mut current, &mut next);
     }
     false
+}
+
+fn check_remainder_simple(divisor: u32, remainder: u32, n: u32) -> Option<bool> {
+    if remainder == 0 {
+        // Trivial
+        Some(true)
+    } else if 10_u32.pow(n) - 1 < remainder {
+        // We can't possibly reach remainder with n digits
+        // Note this includes the case where n == 0 and remainder != 0
+        Some(false)
+    } else if ((divisor as f32).log10().ceil() as u32) < n {
+        // n is large enough that we can guarantee a solution
+        Some(true)
+    } else {
+        // No guarantee either way -- need to use DP
+        None
+    }
 }
