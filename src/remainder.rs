89795bd153fc68dd0ea0845935f953e41ce68499
Hudson Cooper <hncooper96@gmail.com>
2025-01-02 16:02:22 -0800

    significant speedup by explicitly processing from most to least significant digit

--- src/remainder.rs
+++ src/remainder.rs
@@ -15,6 +15,9 @@ pub fn check_remainder(divisor: u32, remainder: u32, n: u32) -> bool {
     let mut next = vec![false; divisor as usize];
     current[0] = true; // 0 is always achievable
 
+    // Precompute 10^k for k from 0 to n-1
+    let powers = precompute_powers(n);
+
     for i in 0..n {
         // Clear the 'next' array for the current iteration
         for entry in next.iter_mut() {
@@ -23,12 +26,13 @@ pub fn check_remainder(divisor: u32, remainder: u32, n: u32) -> bool {
         for rem in 0..divisor {
             if current[rem as usize] {
                 for digit in 0..=9 {
-                    let new_rem = (rem * 10 + digit) % divisor;
+                    let new_n = n - i - 1;
+                    let new_rem = (rem + digit * powers[new_n as usize]) % divisor;
                     // Short circuit if we're guaranteed a solution
                     if let Some(true) = check_remainder_simple(
                         divisor,
                         (remainder + divisor - new_rem) % divisor,
-                        n - i - 1,
+                        new_n,
                     ) {
                         return true;
                     }
@@ -57,4 +61,13 @@ fn check_remainder_simple(divisor: u32, remainder: u32, n: u32) -> Option<bool> 
         // No guarantee either way -- need to use DP
         None
     }
+}
+
+/// Precompute 10^k for k from 0 to n-1
+fn precompute_powers(n: u32) -> Vec<u32> {
+    let mut powers = vec![1; n as usize];
+    for i in 1..n as usize {
+        powers[i] = powers[i - 1] * 10;
+    }
+    powers
 }
